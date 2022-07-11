23ee729ced8e6817d2bc858da87c07b71f31259f
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-07-11 09:32:49 +0100

    used the keyup and windows event method, added a little styling

--- index.js
+++ index.js
@@ -9,3 +9,37 @@ var timer = setInterval(function(){
     }
     countDown -= 1;
 }, 1000);
+
+
+
+//   $(document).ready(function(){
+//       $("button").click(function(){
+//           $("#test").hide();
+//       });
+//   });
+
+
+$("p").bind("click", function(){
+    $("<div>This is a click Event</div>").appendTo("body");
+});
+
+$("p").bind("dblclick", function(){
+    $("<div>This is a double-click Event</div>").appendTo("body");
+});
+
+
+$("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 10 }, "slow");
+    return false;
+  });
+
+  $('a.printPage').click(function(){
+      window.print();
+      return false;
+  });
+
+  let maxlength = 15;
+  $("textarea").keyup(function(){
+      let textlen = maxlength - $(this).val().length;
+      $('#rchars').text(textlen);
+  });
